dc6350b7c6bf1551df0726c77d6dde8040543408
Fabien Boucher <fboucher@redhat.com>
2023-11-26 08:27:26 +0000

    Add Clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use bf_rust;
+use clap::Parser;
 use std::path;
 
-use bf_rust;
+/// Simple program to get the top biggest files in a directory
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path of the directory to search for the biggest files
+    #[arg(short, long)]
+    path: String,
 
-const TOP: usize = 10;
+    /// Number of files to return
+    #[arg(short, long, default_value_t = 10)]
+    count: usize,
+}
 
 fn main() {
-    let path = path::Path::new("/usr/share").to_path_buf();
+    let args = Args::parse();
 
-    let bf = bf_rust::biggest_files_in_dir(path, TOP);
+    let path = path::Path::new(&args.path).to_path_buf();
+
+    let bf = bf_rust::biggest_files_in_dir(path, args.count);
 
     println!("{:?}", bf)
 }
